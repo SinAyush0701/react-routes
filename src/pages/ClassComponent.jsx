d1e0b48be57b8a6a4d053d44167511195f297a30
SinAyush0701 <iayushsingh23@gmail.com>
2023-09-06 10:23:48 +0530

    implemented all basic react

--- src/pages/ClassComponent.jsx
+++ src/pages/ClassComponent.jsx
@@ -2,10 +2,42 @@ import React from "react";
 import BaseHoc from "../hoc/BaseHoc";
 
 class ClassComponent extends React.Component{
+    constructor(props) {
+        super(props);
+        
+        this.state= {
+        name: "ayush",
+        age: 22,
+        };
+        console.log("Constructor");
+    }
+
+    // mounted life cycle method
+    componentDidMount(){
+        console.log("Component did mount suucessfully");
+    }
+
+    // updated life cycle method
+    componentDidUpdate(){
+        console.log("Component got updated successfully");
+    }
+
+    componentWillUnmount(){
+        console.log("Component unmounted suucessfully");
+    }
+
     render() {
+        console.log("Render invoked!!");
         return(
             <div>
                 <p>This is our Class Component</p>
+                <button onClick= {() => 
+                this.setState({
+                    ...this.state,  // using spread operator
+                    age: this.state.age +1,
+                })
+            }> Add 1 to your age </button>
+            <p> {this.state.age} </p>
             </div>
         );
     }
